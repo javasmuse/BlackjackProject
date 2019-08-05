c2f279fc1342dd0cfdec6faa2aafd9918d66458b
Candace <50122081+javasmuse@users.noreply.github.com>
2019-08-05 07:25:35 -0600

    got rid of the unicode
    I got too fancy w/ unicode and it broke / acted weird. Better now.

---
+++ BlackjackProject/BlackjackProject/README.md
@@ -0,0 +1,11 @@
+## Blackjack Project
+
+### Week 4 Homework Project for Skill Distillery
+
+### Overview
+
+### Technologies Used
+
+### How to Run
+
+### Lessons Learned
---
+++ BlackjackProject/BlackjackProject/src/com/skilldistillery/cards/blackjack/Dealer.java
@@ -0,0 +1,41 @@
+package com.skilldistillery.cards.blackjack;
+
+import com.skilldistillery.cards.common.AbstractHand;
+
+public class Dealer extends Person {
+	
+	private AbstractHand hand = new BlackJackHand(); 
+	
+	public Dealer() {
+	}
+
+	
+	
+	
+	public AbstractHand getHand() {
+		return hand;
+	}
+
+
+
+
+	public void setHand(AbstractHand hand) {
+		this.hand = hand;
+	}
+
+
+
+
+	@Override
+	public String toString() {
+		return "\nDealer " + hand;
+	}
+
+
+
+
+	
+	
+	
+
+}
